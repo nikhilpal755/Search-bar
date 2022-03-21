5cd2a9f00248d15c557b0b3dcba1e8c684b0c980
Nikhil Pal <nikhil16175@gmail.com>
2022-03-21 17:30:57 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("")
+
+
+  useEffect(()=>{
+    const fetchUsers = async() =>{
+      const users = await axios.get('https://jsonplaceholder.typicode.com/users');
+      console.log(users.data);
+      setUsers(users.data);
+    }
+    fetchUsers();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+      type="text" 
+      placeholder="Search User"
+      onChange={(e) => setSearch(e.target.value) }/>
+
+      {
+        users?.filter((user) =>{
+          if(search ==""){
+            return user
+          }else if(user?.name.toLowerCase().includes(search.toLowerCase())){
+            return user
+          }
+        }).map((user, key) =>{
+          return <div key={key}>
+            <p>{user.name}</p>
+          </div>
+        })
+        
+          
+    
+      }
     </div>
   );
 }
